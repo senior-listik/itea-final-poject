4e51721dc557c361317042362a3cfdadb2f90742
Max Lystopadov <senior.listik@gmail.com>
2023-04-10 15:45:57 +0300

    change event touch

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -5,7 +5,7 @@ const linkTheme = document.querySelector('[title="theme"]');
 let themeUrl = "./css/day-theme.css"; //path to css file with light styles
 
 dayToggle.addEventListener('click', toggleTheme);
-dayToggle.addEventListener('touchstart', toggleTheme);
+dayToggle.addEventListener('touchend', toggleTheme);
 
 
 // the function of switching styles by connecting a css file with light styles + switching icons
