8417899a95d71607533d3bb9f8e02822f17e1822
Max Lystopadov <senior.listik@gmail.com>
2023-04-10 15:31:11 +0300

    theme switching implementation

--- js/main.js
+++
@@ -1,9 +0,0 @@
-const dayToggle = document.querySelector('.home__toggle');
-
-dayToggle.addEventListener('click', toggleStyles);
-
-function toggleStyles() {
-    dayToggle.classList.toggle('icon-day');
-    dayToggle.classList.toggle('icon-night');
-    
-}
---
+++ js/theme-toggle.js
@@ -0,0 +1,32 @@
+// creating variables
+const dayToggle = document.querySelector('.home__toggle');
+const linkTheme = document.querySelector('[title="theme"]');
+
+let themeUrl = "./css/day-theme.css"; //path to css file with light styles
+
+dayToggle.addEventListener('click', toggleTheme);
+
+// the function of switching styles by connecting a css file with light styles + switching icons
+function toggleTheme() {
+    dayToggle.classList.toggle('icon-day');
+    dayToggle.classList.toggle('icon-night');
+    
+    if (linkTheme.hasAttribute('href')) {
+        linkTheme.removeAttribute('href');
+        localStorage.removeItem('theme');
+    } else {
+        linkTheme.setAttribute('href', themeUrl);
+        localStorage.setItem('theme', 'day'); //saving light theme to local storage
+    }
+    
+}
+
+// checking if light theme exists in local storage and enable it
+let activeTheme = localStorage.getItem('theme');
+
+if (activeTheme === 'day') {
+    linkTheme.setAttribute('href', themeUrl);
+    localStorage.setItem('theme', 'day');
+    dayToggle.classList.toggle('icon-day');
+    dayToggle.classList.toggle('icon-night');
+}
