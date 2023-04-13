c85e1d8f0265faa9dec98d5f1057420cfde482c8
Max Lystopadov <senior.listik@gmail.com>
2023-04-13 15:20:36 +0300

    fixed burger menu + add close icon

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 // burger menu
-const burger = document.querySelector('.icon-burger');
-const navMenu = document.querySelector('.header__nav');
-const menu = document.querySelector('.header__menu');
-burger.addEventListener('click', showMenu);
+const burgerIcon = document.querySelector('.header__icon');
+const headerNav = document.querySelector('.header__nav');
+const headerMenu = document.querySelector('.header__menu');
+
+burgerIcon.addEventListener('click', showMenu);
+headerMenu.addEventListener('click', hideMenu);
 
 function showMenu() {
-    burger.classList.toggle('rotate');
-    navMenu.classList.toggle('visible');
+    // burgerIcon.classList.toggle('rotate');
+    headerNav.classList.toggle('visible');
+    burgerIcon.classList.toggle('icon-close');
+    burgerIcon.classList.toggle('icon-burger')
 }
-menu.addEventListener('click', showMenu);
+function hideMenu() {
+    headerNav.classList.remove('visible');
+    // burgerIcon.classList.toggle('rotate');
+}
+
+
 document.querySelector('main').addEventListener('click', function () {
-    if (navMenu.classList.contains('visible')) {
-        navMenu.classList.remove('visible');
-        burger.classList.toggle('rotate');
+    if (headerNav.classList.contains('visible')) {
+        hideMenu();
     }
 });
