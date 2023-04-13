44e528f6bb3f09c1f4e65543820036bce899b64a
Max Lystopadov <senior.listik@gmail.com>
2023-04-13 14:19:44 +0300

    fixed some styles

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // burger menu
 const burger = document.querySelector('.icon-burger');
 const navMenu = document.querySelector('.header__nav');
-
+const menu = document.querySelector('.header__menu');
 burger.addEventListener('click', showMenu);
 
 function showMenu() {
     burger.classList.toggle('rotate');
     navMenu.classList.toggle('visible');
 }
-navMenu.addEventListener('click', showMenu);
+menu.addEventListener('click', showMenu);
 document.querySelector('main').addEventListener('click', function () {
     if (navMenu.classList.contains('visible')) {
         navMenu.classList.remove('visible');
