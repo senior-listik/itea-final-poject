b1b871fb8b1baef2d7a7c6b5c4cd3a02827018d3
Max Lystopadov <senior.listik@gmail.com>
2023-05-09 15:28:36 +0300

    added  send of form

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+"use strict"
 // burger menu
 const mainElement = document.querySelector('main');
 const burgerIcon = document.querySelector('.header__icon');
@@ -105,7 +106,7 @@ function generatePromoCode() {
         getPromocodeBtn.classList.add('hide');
 
         promocode = ''
-        for (i = 0; i < 8; i++){
+        for (let i = 0; i < 8; i++){
             promocode += Math.round(Math.random() * 10);
         }
         promocodeInfo.innerText = promocode;
@@ -227,4 +228,75 @@ function toggleModalVisible(event) {
     couponModal.classList.toggle('hide');
 }
 
+// send form
 
+// перевірка чи вже завантажилась сторінка
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault(); // забороняємо стандартну відправку форми
+        
+        let error = formValidate(form);
+        let formData = new FormData(form);
+        
+        if (error === 0) {
+            document.querySelector('.reservation').classList.add('_sending');
+            document.body.classList.add('overflowYHidden');
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body: formData
+            }); 
+            if (response.ok) {
+                let result = await response.json();
+                alert(result.message);
+                form.reset();
+                document.querySelector('.reservation').classList.remove('_sending');
+                document.body.classList.remove('overflowYHidden');
+            } else {
+                alert('E R R O R');
+                document.querySelector('.reservation').classList.remove('_sending');
+                document.body.classList.remove('overflowYHidden');
+            }
+        } else {
+            alert('Fill in required fields')
+        }
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+        console.log(formReq)
+
+        for (let i = 0; i < formReq.length; i++){
+            const input = formReq[i];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else {
+                    if (input.value === '') {
+                       formAddError(input);
+                        error++; 
+                    }
+                }
+            }
+        return error;
+    }
+
+    function formAddError(input) {
+        // input.parentElement.classList.add('_error');
+        input.classList.add('_error')
+    }
+    function formRemoveError(input) {
+        // input.parentElement.classList.remove('_error');
+        input.classList.remove('_error')
+    }
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2.8})+$/.test(input.value);
+    }
+})
---
+++ sendmail.php
@@ -0,0 +1,46 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
+$mail->setLanguage('ru', 'phpmailer/language/');
+$mail->IsHTML(true);
+
+$mail->setFrom('pizza_mania@kuku,com', 'Мой проект');
+$mail->addAddress('senior.listik@gmail.com');
+$mail->Subject = 'Приветик! Хочешь пиццы?)';
+
+$body = '<h1>Встречайте супер письмо!</h1>';
+
+if(trim(!empty($_POST['name']))){
+    $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+}
+if(trim(!empty($_POST['email']))){
+    $body.='<p><strong>Е-mail:</strong> '.$_POST['email'].'</p>';
+}
+if(trim(!empty($_POST['number']))){
+    $body.='<p><strong>Количество людей:</strong> '.$_POST['number'].'</p>';
+}
+if(trim(!empty($_POST['text']))){
+    $body.='<p><strong>Дата и время:</strong> '.$_POST['text'].'</p>';
+}
+
+$mail->Body = $body;
+
+if(!$mail->send()) {
+    $message = 'Oшибкa';
+}else{
+    $message = 'Данные отправлены';
+}
+
+$response = ['message' => $message];
+
+header('Content-type: application/json');
+echo json_encode($response);
+
+?>
